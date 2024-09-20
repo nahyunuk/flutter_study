3ddc0d9ac201dae97bfd975b3d8b4652e1a2a11d
nahyunuk <s24022@gsm.hs.kr>
2024-09-20 20:13:03 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day_1/GestureDetector.dart';
+import 'package:day_1/ListView.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Gesturedetector(),
+      home: Listview(),
     );
   }
 }

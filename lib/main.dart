a3adf0718712035facae54f349b1a03ebdc4e139
nahyunuk <s24022@gsm.hs.kr>
2024-09-20 19:14:56 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:day_1/Drawar_FloatingActionButton.dart';
+import 'package:day_1/Column_Row.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Drawar_FloatingActionButton(),
+      home: ColumnRow(),
     );
   }
 }

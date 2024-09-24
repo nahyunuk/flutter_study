21439ba8f5e64308c437bccc3563402ca22da184
nahyunuk <s24022@gsm.hs.kr>
2024-09-24 20:14:26 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day_1/Scroll.dart';
+import 'package:day_1/Align.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scroll(),
+      home:MyAlign(),
     );
   }
 }

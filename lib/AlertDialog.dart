6484fe0dcf40bd4a67e2891f59cf83a8543c0ce4
nahyunuk <s24022@gsm.hs.kr>
2024-09-24 21:07:10 +0900

    study_AlertDiologe

---
+++ lib/AlertDialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Alertdialog extends StatelessWidget {
+  const Alertdialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test title"),
+      ),
+      body: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext con) {
+                  return AlertDialog(
+                    title: const Text("Dialog Title"),
+                    content: Container(
+                      child:
+                          SingleChildScrollView(child: Text("Dialog content")),
+                    ),
+                    actions: [
+                      TextButton(
+                          onPressed: () {},
+                          child: Text("Yes")),
+                      TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Close"))
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text("Button"),
+          ),
+        ),
+      ),
+    );
+  }
+}

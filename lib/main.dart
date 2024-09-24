b8c93caf561093157fd6fab3da2957a0b264ba98
nahyunuk <s24022@gsm.hs.kr>
2024-09-24 21:07:36 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day_1/Align.dart';
+import 'package:day_1/AlertDialog.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:MyAlign(),
+      home:Alertdialog(),
     );
   }
 }

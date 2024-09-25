6e60e45124b6e4e62aac6908d5435d0c116ee431
nahyunuk <s24022@gsm.hs.kr>
2024-09-25 19:59:45 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day_1/Gradient.dart';
+import 'package:day_1/Button.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:MyGradient(),
+      home:Button(),
     );
   }
 }
